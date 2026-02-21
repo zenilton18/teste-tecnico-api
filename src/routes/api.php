d4af1f5b27be7f738838b11a6066122ecc93c4e7
zenilton <ze18.fv@gmail.com>
2026-02-21 13:45:03 -0300

    Configura rotas de API no Laravel 11

---
+++ src/routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\ClienteController;
+use App\Http\Controllers\Api\V1\PropostaController;
+use App\Http\Controllers\Api\V1\AuditoriaPropostaController;
+
+Route::prefix('v1')->group(function () {
+
+    // Clientes
+    Route::post('/clientes', [ClienteController::class, 'store']);
+    Route::get('/clientes/{id}', [ClienteController::class, 'show']);
+
+    // Propostas
+    Route::post('/propostas', [PropostaController::class, 'store']);
+    Route::patch('/propostas/{id}', [PropostaController::class, 'update']);
+
+    Route::post('/propostas/{id}/submit', [PropostaController::class, 'submit']);
+    Route::post('/propostas/{id}/approve', [PropostaController::class, 'approve']);
+    Route::post('/propostas/{id}/reject', [PropostaController::class, 'reject']);
+    Route::post('/propostas/{id}/cancel', [PropostaController::class, 'cancel']);
+
+    Route::get('/propostas/{id}', [PropostaController::class, 'show']);
+    Route::get('/propostas', [PropostaController::class, 'index']);
+
+    // Auditoria
+    Route::get('/propostas/{id}/auditoria', [AuditoriaPropostaController::class, 'index']);
+});
